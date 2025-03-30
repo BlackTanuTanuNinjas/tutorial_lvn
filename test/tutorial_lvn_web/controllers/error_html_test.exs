b283afc8541ce1d986a5cb281b0bf1d9748ed0ae
BlackTanuTanuNinjas <development.tanuki2024@gmail.com>
2025-03-31 00:36:27 +0900

    mix deps.get

--- test/tutorial_lvn_web/controllers/error_html_test.exs
+++ test/tutorial_lvn_web/controllers/error_html_test.exs
@@ -9,6 +9,7 @@ defmodule TutorialLvnWeb.ErrorHTMLTest do
   end
 
   test "renders 500.html" do
-    assert render_to_string(TutorialLvnWeb.ErrorHTML, "500", "html", []) == "Internal Server Error"
+    assert render_to_string(TutorialLvnWeb.ErrorHTML, "500", "html", []) ==
+             "Internal Server Error"
   end
 end
